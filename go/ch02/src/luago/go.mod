a266c586486114a372f931560a782287b4a2ef2a
tianbing <tianbing@kuaishou.com>
2022-05-04 18:51:49 +0800

    delete

--- go/ch02/src/luago/go.mod
+++
@@ -1,5 +0,0 @@
-module tianbingj.github.com/luago
-
-go 1.16
-
-require tianbingj.github.com/binchunk
--- go/ch02/src/luago/main.go
+++ go/ch02/src/luago/main.go
@@ -1,18 +1,18 @@
 package main
 
-import (
-	"io/ioutil"
-	"os"
-	"tianbingj.github.com/binchunk"
-)
-
-func main() {
-	if len(os.Args) > 1 {
-		data, err := ioutil.ReadFile(os.Args[1])
-		if err != nil {
-			panic(err)
-		}
-		proto := binch
-	}
-
-}
+//import (
+//	"io/ioutil"
+//	"os"
+//	//"tianbingj.github.com/binchunk"
+//)
+//
+//func main() {
+//	if len(os.Args) > 1 {
+//		data, err := ioutil.ReadFile(os.Args[1])
+//		if err != nil {
+//			panic(err)
+//		}
+//		proto := binch
+//	}
+//
+//}
