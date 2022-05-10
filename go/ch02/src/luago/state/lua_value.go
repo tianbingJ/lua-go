868c877f2148fed01ea5c89273e4793f6011e6e4
tianbing <tianbing@kuaishou.com>
2022-05-10 20:38:43 +0800

    add arith

--- go/ch02/src/luago/state/lua_value.go
+++ go/ch02/src/luago/state/lua_value.go
@@ -2,14 +2,15 @@ package state
 
 import (
 	"github.com/tianbingJ/lua-go/lua-dump/api"
+	"github.com/tianbingJ/lua-go/lua-dump/number"
 )
 
-type luaValue interface {}
+type luaValue interface{}
 
 func typeOf(val luaValue) api.LuaType {
 	switch val.(type) {
-	case nil :
-		return api.LUA_TNIL;
+	case nil:
+		return api.LUA_TNIL
 	case bool:
 		return api.LUA_TBOOLEAN
 	case int64:
@@ -21,4 +22,40 @@ func typeOf(val luaValue) api.LuaType {
 	default:
 		panic("todo!")
 	}
+}
+
+func convertToFloat(val luaValue) (float64, bool) {
+	switch x := val.(type) {
+	case float64:
+		return x, true
+	case int64:
+		return float64(x), true
+	case string:
+		return number.ParseFloat(x)
+	default:
+		return 0, false
+	}
+}
+
+func convertToInteger(val luaValue) (int64, bool) {
+	switch x := val.(type) {
+	case int64:
+		return x, true
+	case float64:
+		return number.FloatToInteger(x)
+	case string:
+		return _stringToInteger(x)
+	default:
+		return 0, false
+	}
+}
+
+func _stringToInteger(str string) (int64, bool) {
+	if i, ok := number.ParseInteger(str); ok {
+		return i, true
+	}
+	if f, ok := number.ParseFloat(str); ok {
+		return number.FloatToInteger(f)
+	}
+	return 0, false
 }
